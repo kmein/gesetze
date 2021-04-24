4b9945481bc152d57053af0dd0b33dc48c40904b
Kierán Meinhardt <kmein@posteo.de>
2021-04-24 16:17:38 +0200

    feat(shell.nix): improve performance by using recht dump

--- shell.nix
+++ shell.nix
@@ -1,30 +1,15 @@
 { pkgs ? import <nixpkgs> {} }:
 let
   recht-src = builtins.fetchTarball {
-    url = "https://github.com/kmein/recht/archive/0.4.1.tar.gz";
-    sha256 = "1z783qn46divf2zbixwhfwbxwp8nyd0drzsfcxdyxp3mnsb67ap5";
+    url = "https://github.com/kmein/recht/archive/0.5.0.tar.gz";
+    sha256 = "0wcy286dfia4xw3lbbq2j8qprdwr5s76v27hjxm5l8231wslmv4z";
   };
   recht = pkgs.callPackage recht-src {};
 in
 pkgs.mkShell {
   buildInputs = [
     (pkgs.writers.writeBashBin "generate" ''
-      > index.md
-
-      ${recht}/bin/recht list \
-      | ${pkgs.ansifilter}/bin/ansifilter \
-      | ${pkgs.glibc.bin}/bin/iconv -t utf8//IGNORE \
-      | while read -r law; do
-        law_title="''${law##*] }"
-        law_abbreviation="$(echo $law | ${pkgs.gnugrep}/bin/grep -o '^\[.*]' | ${pkgs.gnused}/bin/sed 's/[][]//g')"
-        law_path="laws/''${law_abbreviation//\//_}.txt"
-
-        ${recht}/bin/recht get "$law_abbreviation" \
-        | ${pkgs.ansifilter}/bin/ansifilter \
-        | ${pkgs.glibc.bin}/bin/iconv -t utf8//IGNORE > "$law_path"
-
-        echo "- [$law_abbreviation](''${law_path// /%20}) $law_title" | tee -a index.md
-      done
+      ${recht}/bin/recht dump laws | tee index.md
     '')
   ];
 }
