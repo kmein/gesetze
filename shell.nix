6abb0e4b57810f01169c1a80e2c9cbc865f17d12
Kierán Meinhardt <kmein@posteo.de>
2021-04-25 13:43:34 +0200

    feat: update recht to generate markdown

--- shell.nix
+++ shell.nix
@@ -1,14 +1,15 @@
 { pkgs ? import <nixpkgs> {} }:
 let
   recht-src = builtins.fetchTarball {
-    url = "https://github.com/kmein/recht/archive/0.5.0.tar.gz";
-    sha256 = "0wcy286dfia4xw3lbbq2j8qprdwr5s76v27hjxm5l8231wslmv4z";
+    url = "https://github.com/kmein/recht/archive/0.6.0.tar.gz";
+    sha256 = "0kl5669p609yq8js4y5r2gjzrsw2r7qi55hr69kb9agc683pcixn";
   };
   recht = pkgs.callPackage recht-src {};
 in
 pkgs.mkShell {
   buildInputs = [
     (pkgs.writers.writeBashBin "generate" ''
+      rm -rf laws
       ${recht}/bin/recht dump laws | tee index.md
     '')
   ];
